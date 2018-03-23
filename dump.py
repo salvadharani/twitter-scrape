1a3ded4d501e64d95184411f5c212d0d32232d53
Salva Dharanikumar <salvadharani.84@gmail.com>
2018-03-23 09:20:40 +0530

    Update dump.py

--- dump.py
+++ dump.py
@@ -4,6 +4,6 @@ import dataset
 from textblob import TextBlob
 
 db = dataset.connect(settings.CONNECTION_STRING)
-
 result = db[settings.TABLE_NAME].all()
+
 dataset.freeze(result, format='csv', filename=settings.CSV_NAME)
